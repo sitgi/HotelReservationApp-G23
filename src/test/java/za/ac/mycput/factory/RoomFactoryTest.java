93792345ca6a30186f4332934aa76aa593020686
SbongaS <219143188@mycput.ac.za>
2024-03-27 20:05:25 +0200

    RoomUpdate

---
+++ src/test/java/za/ac/mycput/factory/RoomFactoryTest.java
@@ -0,0 +1,14 @@
+package za.ac.mycput.factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomFactoryTest {
+
+    @org.junit.jupiter.api.Test
+    void buildRoom() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buildRoomWithGeneratedNumber() {
+    }
+}
